6b85156458839326f5734ffefa36f2fa29be0bb0
Shawn Neal <sneal@sneal.net>
2013-03-29 21:51:11 -0700

    Added windows cookbook as a dependency in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      "Installs/Configures .NET Framework"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 supports         "windows"
+depends          "windows", ">= 1.2.6"
