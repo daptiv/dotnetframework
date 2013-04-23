3ce5e77d06db56ca25834bec85dc2e0ed76f5177
Yvonne Lam <ylam@daptiv.com>
2013-04-22 17:31:07 -0700

    Use single-quoted strings per extended foodcritic rules.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             "dotnetframework"
-maintainer       "Daptiv Solutions, LLC"
-maintainer_email "sneal@daptiv.com"
-license          "All rights reserved"
-description      "Installs/Configures .NET Framework"
+name             'dotnetframework'
+maintainer       'Daptiv Solutions, LLC'
+maintainer_email 'sneal@daptiv.com'
+license          'All rights reserved'
+description      'Installs/Configures .NET Framework'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-supports         "windows"
-depends          "windows", ">= 1.2.6"
+version          '0.1.0'
+supports         'windows'
+depends          'windows', '>= 1.2.6'
