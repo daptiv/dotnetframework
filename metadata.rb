5b95cd72315063c1ed0d6958ad24a4ff98b7043d
Yvonne Lam <ylam@daptiv.com>
2013-04-23 17:21:19 -0700

    teamcity can set patch version.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'sneal@daptiv.com'
 license          'All rights reserved'
 description      'Installs/Configures .NET Framework'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          (ENV['BUILD_NUMBER'] ? "0.1.#{ENV['BUILD_NUMBER']}" : '0.1.0')
+#version          '0.1.0'
 supports         'windows'
 depends          'windows', '>= 1.2.6'
