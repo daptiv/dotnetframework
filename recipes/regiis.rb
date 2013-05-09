95760e6eafd25ff6acf93a360498cf69813d7ca0
Shawn Neal <sneal@sneal.net>
2013-05-09 15:24:02 -0700

    Fixed extended foodcritic linting error

--- recipes/regiis.rb
+++ recipes/regiis.rb
@@ -33,7 +33,7 @@ dotnet4dir = File.join(ENV['WINDIR'], 'Microsoft.Net\\Framework64\\v4.0.30319')
 aspnet_regiis_cmd = File.join(dotnet4dir, 'aspnet_regiis.exe')
 appcmd = File.join(ENV['WINDIR'], 'system32\\inetsrv\\appcmd.exe')
 
-execute "aspnet_regiis" do
+execute 'aspnet_regiis' do
   command "#{aspnet_regiis_cmd} -i"
   action :run
   not_if do
