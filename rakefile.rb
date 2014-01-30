99b17928d33987a3a61b53635372617b7c3536fa
Shawn Neal <sneal@sneal.net>
2014-01-30 12:44:06 -0800

    This cookbook's default version starts at 0.1.0

--- rakefile.rb
+++ rakefile.rb
@@ -6,7 +6,7 @@ task :lint => [:version, :tailor, :foodcritic]
 task :default => [:lint]
 
 task :version do
-  IO.write('version.txt', (ENV['BUILD_NUMBER'] ? "0.0.#{ENV['BUILD_NUMBER']}" : '0.0.1'))
+  IO.write('version.txt', (ENV['BUILD_NUMBER'] ? "0.0.#{ENV['BUILD_NUMBER']}" : '0.1.0'))
 end
 
 FoodCritic::Rake::LintTask.new do |t|
