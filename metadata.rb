9ceea67678d6b85cbf13a141f089d23e0d039a2e
Shawn Neal <sneal@sneal.net>
2014-01-29 16:04:22 -0800

    Read cookbook version from version.txt

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sneal@daptiv.com'
 license          'All rights reserved'
 description      'Installs/Configures .NET Framework'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          (ENV['BUILD_NUMBER'] ? "0.1.#{ENV['BUILD_NUMBER']}" : '0.1.0')
-#version          '0.1.0'
+ver_path = File.join(File.dirname(__FILE__), 'version.txt')
+version ((IO.read(ver_path) if File.exists?(ver_path)) || '0.0.1').chomp
 supports         'windows'
 depends          'windows', '>= 1.2.6'
