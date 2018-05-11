36cb4e17cc6a7364cfdd124ecc99bde530a48655
Heath Snow <heathsnow@gmail.com>
2018-05-11 15:18:10 -0700

    bump chef_version requirement to 13.0 as that is what we test

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures .NET Framework'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
 version File.exist?(ver_path) ? IO.read(ver_path).chomp : '1.3.0'
-chef_version '>= 12.5' if respond_to?(:chef_version)
+chef_version '>= 13.0' if respond_to?(:chef_version)
 issues_url 'https://github.com/daptiv/dotnetframework/issues'
 source_url 'https://github.com/daptiv/dotnetframework/'
 supports 'windows'
